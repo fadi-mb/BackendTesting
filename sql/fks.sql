7c04de3e7904d1c2953c62bc7b5778b59e49aadb
Rami kass youssef <rami.k.youssef@gmail.com>
2020-09-17 19:52:40 +0300

    script fix

--- sql/fks.sql
+++ sql/fks.sql
@@ -2,5 +2,5 @@ ALTER TABLE `companies`
   ADD CONSTRAINT `fk_companies_users` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
 
 ALTER TABLE `users`
-  ADD CONSTRAINT `fk_user_people` FOREIGN KEY (`person_id`) REFERENCES `people` (`person_id`),
+  -- ADD CONSTRAINT `fk_user_people` FOREIGN KEY (`person_id`) REFERENCES `people` (`person_id`),
   ADD CONSTRAINT `fk_users_companies` FOREIGN KEY (`company_id`) REFERENCES `companies` (`id`);
