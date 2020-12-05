80220d1fb9f6085b3a50832b7d3b4ac61d47733b
fadi barakat <fadi.mo.barakat@gmail.com>
2020-12-05 23:15:53 +0200

    measurements done

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 process.env.TZ = 'UTC'
 const express = require('express');
+const { query } = require('express-validator');
 const app = express();
 const http = require('http');
 
@@ -7,12 +8,40 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const port = 12345;
+const idsRegex = /^(\d{1,8},)*\d{1,8}$/;
 const httpServer = http.createServer(app);
 
 const middleware = require('./middleware');
 
 app.get('/performance', middleware.testPerformance);
-app.get('/measurements', middleware.measurements);
+app.get('/measurements', [
+    query('devices')
+        .exists().withMessage('Device IDs must be provided!')
+        .matches(/^(\d{1,8},)*\d{1,8}$/).withMessage('Invalide device IDs!')
+    // .custom(value => {
+    //     console.log(`custom ${value}`);
+    //     console.log(value);
+    //     if (value) {
+    //         return idsRegex.test(value);
+    //     }
+    //     return false;
+    // }
+    // )
+], middleware.measurements);
+
+
+app.use((error, req, res, next) => {
+    if (error.statusCode) {
+        res.status(error.statusCode).json({
+            message: error.message
+        })
+    } else {
+        res.status(500).json({
+            message: 'Some error occurred!,sorry for the inconvenience!'
+        })
+    }
+
+});
 
 httpServer.listen(port, () => {
     console.log(`Listening on port ${port}`);
--- src/middleware.js
+++ src/middleware.js
@@ -1,3 +1,5 @@
+const { validationResult } = require('express-validator');
+
 const knex = require("knex")({
     client: "mysql",
     connection: {
@@ -6,6 +8,7 @@ const knex = require("knex")({
         user: process.env.dbuser,
         password: process.env.dbpassword,
         database: process.env.dbname,
+        debug: ['ComQueryPacket'],
         typeCast: function castField(field, useDefaultTypeCasting) {
             if ((field.type === "BIT") && (field.length === 1)) {
                 var bytes = field.buffer();
@@ -15,27 +18,14 @@ const knex = require("knex")({
         }
     }
 });
-function groupBy(list, keyGetter) {
-    const map = new Map();
-    list.forEach((item) => {
-        const key = keyGetter(item);
-        const collection = map.get(key);
-        if (!collection) {
-            map.set(key, [item]);
-        } else {
-            collection.push(item);
-        }
-    });
-    return map;
-}
+
 exports.testPerformance = [
     async (req, res, next) => {
         try {
-            //Retraiving all required data by one query
-            //select `companies`.`id`, `companies`.`name`, `users`.`email`, `users`.`id` as `u_id`, `users`.`phone` from `companies` inner join `users` on `users`.`company_id` = `companies`.`id` order by `companies`.`id` asc
-            const rawData = await knex.select(['companies.id', 'companies.name', 'users.email', 'users.id as u_id', 'users.phone']).orderBy('companies.id').from('companies').leftJoin('users', 'users.company_id', 'companies.id');
+            //Retrieving all required data by one query
+            const rawData = await knex.select('companies.id', 'companies.name', 'users.email', 'users.id as u_id', 'users.phone').orderBy('companies.id').from('companies').leftJoin('users', 'users.company_id', 'companies.id');
 
-            //Transforming data to a tree structure 
+            //Transforming data into nested objects structure 
             const map = new Map();
             rawData.forEach((item) => {
                 const key = item.id;
@@ -65,9 +55,56 @@ exports.testPerformance = [
 exports.measurements = [
     async (req, res, next) => {
         try {
-            const reuslt = {}
+            const errors = validationResult(req);
+            if (!errors.isEmpty()) {
+                console.log(errors.errors);
+                const error = new Error(`Validation failed, ${errors.errors[0].msg}.`);
+                error.statusCode = 422;
+                throw error;
+            }
+            //Convert devices IDs from comma separeted values to a number array
+            const devices = req.query.devices.split(',').map(id => +id);
+
+            const lastReads =
+                await knex(
+                    knex('iot_measurements').whereIn('device_id', devices).select('device_id', 'sensor_id').max('id as id').groupBy(['device_id', 'sensor_id']).as('t1')
+                ).innerJoin('iot_measurements as t2', 't1.id', 't2.id')
+                    .select('t2.device_id', 't2.sensor_id', 't2.alarm_id', 't2.value', 't2.status', 't2.created_at');
+
+            const rawSensorsStatus =
+                await knex('iot_measurements').whereIn('device_id', devices).select('device_id', 'sensor_id', 'status', knex.raw('COUNT(DISTINCT(alarm_id)) as reported'))
+                    .groupBy('device_id', 'sensor_id', 'status')
+
+            //Transforming sensorsStatus data to a meet the form requested 
+            const map = new Map();
+            rawSensorsStatus.forEach((item) => {
+                const key = item.device_id.toString() + '_' + item.sensor_id.toString();
+                let sensor = map.get(key);
+                if (!sensor) {
+                    sensor = {
+                        device_id: item.device_id,
+                        sensor_id: item.sensor_id,
+                        good: 0,
+                        poor: 0,
+                        problem: 0,
+                        ok: 0
+                    };
+                    map.set(key, sensor);
+                }
+                switch (item.status) {
+                    case 'good': sensor.good = item.reported; break;
+                    case 'poor': sensor.poor = item.reported; break;
+                    case 'problem': sensor.problem = item.reported; break;
+                    case 'ok': sensor.ok = item.reported; break;
+                    default: break;
+                }
+            });
+            const sensorsStatus = [...map.values()];
+
+            const reuslt = { lastReads: lastReads, sensorsStatus: sensorsStatus }
             res.status(200).json(reuslt);
         } catch (err) {
+            console.log(err);
             next(err);
         }
     }
